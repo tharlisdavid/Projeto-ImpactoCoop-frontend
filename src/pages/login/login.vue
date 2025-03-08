87fcf7deb8f202c9471e5139cb0933ae30ee9b2e
fragoso <fragosoph.dev@gmail.com>
2025-03-08 15:38:49 -0400

    alteração tela de login

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="hHh lpR fFf">
     <!-- Barra superior -->
-    <q-header elevated style="background-color: #145A31; min-height: 60px;">
+    <q-header elevated style="background-color: #306241; min-height: 60px;">
       <q-toolbar>
         <q-toolbar-title style="text-align: center; font-size: xx-large; justify-content: center;">
           ImpactoCoop
@@ -10,52 +10,47 @@
     </q-header>
 
     <!-- Corpo da página -->
-    <q-page-container>
-      <q-page class="flex flex-center" style="align-items: center; gap: 8rem;">
-
-        <q-card-section>
-          <div>
-            <img src="/src/assets/icon.svg" alt="form" style="width: 460px;">
+    <q-page-container class="container">
+      <q-card class="img-content">
+        <div>
+          <img src="/src/assets/icone-tela-login.svg" alt="form" id="img-tela-login">
+        </div>
+      </q-card>
+      <q-card class="form-content">
+        <div class="form-card">
+          <div class="title">
+            <h3>Bem Vindo</h3>
           </div>
-        </q-card-section>
+          <q-form @submit="onLogin" style="display: flex; flex-direction: column;">
+            <q-input
+              v-model="email"
+              label="Email"
+              type="email"
+              :rules="[val => val && val.length > 0 || 'Por favor insira um email']"
+              class="input"
+            />
 
-        <q-card class="q-pa-md" style="width: 480px;">
-          <q-card-section>
-            <div class="text-h4 text-center" style="text-align: center; font-weight: bold; color: #000; letter-spacing: 2px;">Bem Vindo</div>
-          </q-card-section>
-          <q-card-section>
-            <q-form @submit="onLogin">
-              <!-- Input para o email -->
-              <q-input
-                v-model="email"
-                label="Email"
-                type="email"
-                :rules="[val => val && val.length > 0 || 'Por favor insira um email']"
-                class="q-mb-md"
-              />
+            <!-- Input para a senha -->
+            <q-input
+              v-model="senha"
+              label="Senha"
+              type="password"
+              :rules="[val => val && val.length > 0 || 'Por favor insira sua senha']"
+              class="input"
+            />
 
-              <!-- Input para a senha -->
-              <q-input
-                v-model="senha"
-                label="Senha"
-                type="password"
-                :rules="[val => val && val.length > 0 || 'Por favor insira sua senha']"
-                class="q-mb-md"
-              />
-
-              <!-- Botão de login -->
-              <div class="q-pa-md q-gutter-sm">
-                <q-btn :loading="loading[1]" class="btn-button" @click="simulateProgress(1)">
-                  Button
-                  <template v-slot:loading>
-                    Loading...
-                  </template>
-                </q-btn>
-              </div>
-            </q-form>
-          </q-card-section>
-        </q-card>
-      </q-page>
+            <!-- Botão de login -->
+            <div class="btn-button">
+              <q-btn :loading="loading[1]" class="btn-button" @click="simulateProgress(1)" style="background-color: #306241; color: white;">
+                Entrar
+                <template v-slot:loading>
+                  Loading...
+                </template>
+              </q-btn>
+            </div>
+          </q-form>
+        </div>
+      </q-card>
     </q-page-container>
   </q-layout>
 </template>
@@ -91,32 +86,62 @@ export default {
 };
 </script>
 
-<style scoped>
-.q-page {
-  /* background-image: linear-gradient(70deg, #00642b, #03441f) ; */
-  background-color: #fff;
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  /* box-sizing: border-box; */
 }
 
-.q-card {
-  height: 540px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  background-color: #f1f1f1;
-  border-radius: 25px;
-  gap: 2rem;
-  padding: 2rem;
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  background-color: #C0D7BC;
+  height: 100vh;
+  gap: 8%;
+}
+
+#img-tela-login {
+  background-color: #fff;
+  height: 92vh;
+  width: 60vw;
+}
+
+.form-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
+  width: 24vw;
+  background-color: #fff;
+  /* padding: 20px; */
+}
+
+.form-card .input{
+  width: 100%;
+}
+
+.form-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .btn-button {
-  height: 45px;
   width: 100%;
-  background-color: #145A31;
-  color: #fff;
-  border-radius: 15px;
-  align-items: center;
-  transition: background 0.8s ease-in-out; /* Suaviza a transição do hover */
+  display: flex;
+  justify-content: center;
 }
 
-.btn-button:hover {
-  background-image: linear-gradient(to left, #00C759, #145A31, #06782C); /* Corrigido: utilizando background-image */
+.btn-button .q-btn:hover {
+  background-image: linear-gradient(45deg, #275956, #289c66, #073c39);
+  background-size: 180%;
+  transition: 0.8s;
+}
+
+.btn-button .q-btn {
+  width: 100%;
 }
 </style>
